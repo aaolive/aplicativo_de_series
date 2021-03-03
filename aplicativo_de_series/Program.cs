78cabd6452a3413e6f5cedd6a5cced0677e44c7d
Anderson <anderson.assis@outlook.com>
2021-03-02 22:07:29 -0300

    Fiz pequenos ajustes em alguns metodos para exibir infomacao

--- aplicativo_de_series/Program.cs
+++ aplicativo_de_series/Program.cs
@@ -6,7 +6,59 @@ namespace aplicativo_de_series
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            // objeto de serie
+            var rodaAppSerie = new Serie();
+            // crio exemplos de series internamente
+            rodaAppSerie.CriarSeriesDeExemplo();
+            
+            //rodaAppSerie.AdicionarSerie();
+
+            // opcoes de uso do aplicativo
+            int adicionarUmaSerie = 1;
+            int mostrarSeries = 2;
+            int alterarUmaSerie = 3;
+            int excluirUmaSerie = 4;
+            bool fecharAplicativo = false;
+            // faz a interação pegando as escolhas do usuario
+            while (fecharAplicativo == false)
+            {
+                //limpa tela antes de continuar
+                Console.Clear();
+                // exibe mensagem do app
+                Console.WriteLine("....Aplicativo de series....\n");
+                Console.WriteLine("Escolha uma das opcoes para executar: ");
+                // recebe a opcao do usuario
+                var resposta = Console.ReadLine();
+                var opccaoDesejada = Convert.ToInt32(resposta);
+
+                // faz a selecao conforme o desejo do usuario
+                switch (opccaoDesejada)
+                {
+                    case 1:
+                        rodaAppSerie.AdicionarSerie();
+                        break;
+                    case 2:
+                        rodaAppSerie.MostrarSeries();
+                        break;
+                    case 3:
+                        rodaAppSerie.AlterarSerie();
+                        break;
+                    case 4:
+                        rodaAppSerie.ExcluirSerie();
+                        break;
+                    case 5:
+                        if (opccaoDesejada == 5)
+                        {
+                            fecharAplicativo = true;
+                        }
+                        break;                    
+                }
+
+            }
+            // pede confirmação para continuar
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.Read();
         }
     }
 }
--- aplicativo_de_series/Serie.cs
+++ aplicativo_de_series/Serie.cs
@@ -7,7 +7,7 @@ namespace aplicativo_de_series
     class Serie : ISeries
     {
         // uma lista de series
-        private List<Serie> minhaListaDeSeries;
+        private List<Serie> listaDeSeries = new List<Serie>();
 
         // campos de dados
         public string Nome { get; set; }
@@ -23,37 +23,186 @@ namespace aplicativo_de_series
 
         public void AdicionarSerie()
         {
-            // crio um objeto de serie e preencho os dados
+            
+            // limpa tela antes de exibir
+            Console.Clear();
+            // mensagem
+            Console.WriteLine("Adicionando series\n");
+            // crio um objeto de serie e preencho os dados            
             Serie novaSerie = new Serie();
+            // recebe nome
             Console.WriteLine("Digite os dados para sua série");
             Console.Write("Nome: ");
             novaSerie.Nome = Console.ReadLine();
-            Console.WriteLine();
+            // recebe descricao
             Console.Write("Descricao: ");
             novaSerie.Descricao = Console.ReadLine();
-            Console.WriteLine();
+            // recebe o ano
             Console.Write("Ano: ");
             novaSerie.Ano = Console.ReadLine();
-            Console.WriteLine("EWscolha um genero para sua série:");
-            foreach (var numero in Enum.GetValues(typeof(Generos)) )
+            // recebe o genero
+            Console.WriteLine("Escolha o número do genero para sua série:");
+            foreach (int numero in Enum.GetValues(typeof(Generos)) )
             {
-                Console.Write($"{Enum.GetName(typeof(Generos), numero)} = {numero}, ");
+                Console.Write($"{numero}: {Enum.GetName(typeof(Generos), numero)} ");
             }
+            Console.WriteLine();
+            Console.Write("Genero: ");
+            novaSerie.Genero = (Generos)int.Parse(Console.ReadLine());
+            // salvo a serie na lista
+            listaDeSeries.Add(novaSerie);
+            // mando uma confimação
+            Console.WriteLine("A série foi adicionda.");
+
+            // pede confirmação para continuar
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadKey();
         }
 
         public void AlterarSerie()
         {
-            throw new NotImplementedException();
+            // limpa tela antes de exibir
+            Console.Clear();
+
+            // mostra as series
+            Console.WriteLine("Lista de series\n");
+            MostrarSeries();
+
+            // exibindo mensagem
+            Console.WriteLine("Escolha a serie para alterar pelo nome");
+            string nomeDaSerieParaAlterar;
+            nomeDaSerieParaAlterar = Console.ReadLine();            
+
+            // procuro a serie para modificar 
+            foreach (var serie in listaDeSeries)
+            {
+                // caso encontre a serie pede os novos dados
+                if (serie.Nome == nomeDaSerieParaAlterar)
+                {
+                    // variaveis para receber novos dados
+                    string novoNome;
+                    string novaDescricao;
+                    string novoAno;
+                    Generos novoGenero;
+
+                    //recebe o novo nome
+                    Console.WriteLine("Digite o novo nome");
+                    Console.Write("Novo nome: ");
+                    novoNome = Console.ReadLine();
+
+                    //recebe o nova descricao
+                    Console.WriteLine("Digite o nova decricao");
+                    Console.Write("Nova descrição: ");
+                    novaDescricao = Console.ReadLine();
+
+                    //recebe o novo ano
+                    Console.WriteLine("Digite o novo ano");
+                    Console.Write("Novo ano: ");
+                    novoAno = Console.ReadLine();
+
+                    // recebe o novo genero
+                    //Console.WriteLine("Escolha o número do genero para sua série:");
+                    //foreach (int numero in Enum.GetValues(typeof(Generos)))
+                    //{
+                    //    Console.Write($"{numero}: {Enum.GetName(typeof(Generos), numero)} ");
+                    //}
+                    //Console.WriteLine();
+                    //Console.Write("Genero: ");
+                    //novoGenero = (Generos)int.Parse(Console.ReadLine());
+
+                    // preenche com os novos dados caso necessario
+                    if (novoNome.Length > 0)
+                    {
+                        serie.Nome = novoNome;
+                    }
+                    if (novaDescricao.Length > 0)
+                    {
+                        serie.Descricao = novaDescricao;
+                    }
+                    if (novoAno.Length > 0)
+                    {
+                        serie.Ano = novoAno;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("A série não foi encontrada.");
+                }
+            }
+            // pede confirmação para continuar
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadKey();
         }
 
+        // exclui uma serie pelo nome
         public void ExcluirSerie()
         {
-            throw new NotImplementedException();
+            // limpa tela antes de exibir
+            Console.Clear();
+            // mostrando as series primeiro
+            MostrarSeries();
+
+            // campo de pesquisa
+            string nomeParaPesquisar;
+            Console.WriteLine("Digite o nome para pesquisar");
+            Console.Write("Nome: ");
+            nomeParaPesquisar = Console.ReadLine();
+            foreach (var serie in listaDeSeries)
+            {
+                // se achou pelo nome irá remover
+                if (nomeParaPesquisar.ToLower() == serie.Nome.ToLower())
+                {
+                    // recebe o indice do objeto
+                    var indiceDaSerie = listaDeSeries.IndexOf(serie);
+                    listaDeSeries.RemoveAt(indiceDaSerie);
+                    Console.WriteLine("Série removida");
+                }
+                else
+                {
+                    Console.WriteLine("Não foi encontrada uma série com esse nome.");
+                }
+            }
+            // pede confirmação para continuar
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadKey();
         }
 
         public void MostrarSeries()
         {
-            throw new NotImplementedException();
+            // limpa tela antes de exibir
+            Console.Clear();
+            Console.WriteLine("Monstrando series\n");
+            // mostra as series na lista
+            foreach (var serie in listaDeSeries)
+            {
+                Console.WriteLine($"# Serie: {serie.Nome}\n  Descrição: {serie.Descricao}\n  Genero: {serie.Genero}\n  Ano: {serie.Ano}\n");
+            }
+            // pede confirmação para continuar
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadKey();
+        }
+
+        // metodo para criar series  de exemplo
+        public void CriarSeriesDeExemplo()
+        {
+            // exemplos
+            var serie1 = new Serie();
+            var serie2 = new Serie();            
+
+            serie1.Nome = "Supernatural";
+            serie1.Descricao = "A série segue os irmãos Sam e Dean Winchester enquanto procuram por seu pai, John, que está caçando o demônio...";
+            serie1.Ano = "2005";
+            serie1.Genero = Generos.Terror;
+
+            serie2.Nome = "Stranger Things";
+            serie2.Descricao = "Will, um garoto de 12 anos, desaparece em Montauk, Long Island. Enquanto a polícia, família e amigos procuram respostas,...";
+            serie2.Ano = "2016";
+            serie2.Genero = Generos.Suspense;
+
+            // põe as series na lista
+            listaDeSeries.Add(serie1);
+            listaDeSeries.Add(serie2);
         }
     }
 }
